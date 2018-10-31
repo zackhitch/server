68a28085bfa4238b859a6fb4e6f4ab087f55f9e9
zackhitch <zmhitchcock+git@gmail.com>
2018-10-31 09:46:54 -0700

    change server name

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ server.use(helmet());
 server.use(express.json());
 
 server.get('/', (req, res) => {
-	res.status(200).json({ server: 'running!' });
+	res.status(200).json({ api: 'running!' });
 });
 
 module.exports = server;
